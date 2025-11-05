10fc455c323ec02711d2371a68d8dc297c26fd9a
Shane B. <dotfortun@users.noreply.github.com>
2025-11-05 23:57:18 +0000

    Finished up data types, did basic intro to window and document.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let laundryFiles = [
+  {
+    title: "A Conventional Boy",
+    author: "Charles Stross",
+    pages: 211,
+    year_published: 2024,
+  },
+  {
+    title: "Dead Not Dreaming",
+    author: "Charles Stross",
+    pages: 211,
+    year_published: 2022,
+  },
+  {
+    title: "The Fuller Memorandum",
+    author: "Charles Stross",
+    pages: 211,
+    year_published: 2015,
+  },
+];
+
+/**
+ * We have two special objects that we will be using:
+ * window - which gives us an interface to interact with
+ * the browser window
+ * document - which gives us an interface to interact with
+ * the HTML document (aka the Document Object Model.)
+ */
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.body.innerHTML = "<h1>Hello world (from JS).</h1>";
 };
--- src/intro.js
+++ src/intro.js
@@ -1,10 +1,9 @@
 // Strings: Basically just friendship bracelets of characters.
-
 let someVariable = "This is a variable.";
 const someConst = "This value can't be changed ever.";
 
 let someString = "Strings come in a number of types.";
-let someOtherString = 'Strings can use either single or double quotes.';
+let someOtherString = "Strings can use either single or double quotes.";
 let someStrLiteral = `String literals can have any non-backtick character in them.
 Including break returns (newline characters)
 And other "type" of 'quotemarks.'`;
@@ -12,8 +11,8 @@ And other "type" of 'quotemarks.'`;
 // You can even use emoji in HTML and JS:
 let blackCat = "🐈‍⬛";
 let catEmoticon = `╱|、
-(˚ˎ 。7  
-|、˜〵          
+(˚ˎ 。7
+|、˜〵
 じしˍ,)ノ`;
 
 let someStrFormatingLit = `You can interpolate string literals with:
@@ -29,34 +28,31 @@ ${catEmoticon}
 /**
  * This is a block comment, which is way
  * to denote a larger comment.
- * 
+ *
  * Used before a function, it becomes a JSDoc.
  * (I'll demonstrate that on Wednesday.)
  */
 
 // Numbers - Notably *not* text.
-
 let someNumber = 90210;
 let someInteger = 1123581321;
 let someFloat = 1123581321.5;
 let someNaN = NaN;
 
 // Booleans - The answer to all yes or no questions.
-
 let someBool = true;
 let theOtherBool = !someBool;
 
-let someConditional = ("is this string" === "this other string?");
+let someConditional = "is this string" === "this other string?";
 
 // Nullish values - How do you represent nothing?
-
 let someNull = null;
 let someUndefined = undefined;
 let someUninitializeVariable; // This is undefined.
 
 /**
  * Truthiness:
- * 
+ *
  * JS only has 6 falsy values (for now)!
  * (Everything else is truthy!)
  */
@@ -67,3 +63,50 @@ let falsy2 = false;
 let falsy3 = NaN;
 let falsy4 = 0;
 let falsy5 = "";
+
+// Objects - A way to represent things.
+let someObj = {
+  // key: value pairs separated by commas
+  a: "Some random value.",
+  b: 3.1415,
+  c: null,
+};
+
+let book = {
+  title: "A Conventional Boy",
+  author: "Charles Stross",
+  pages: 211,
+  year_published: 2024,
+};
+
+let bookTalk = `We can access properties with dot notation!
+so book.title will output "${book.title}"`;
+
+// Arrays - A way to represent sets/groups of things.
+let someArr = [
+  "The Laundry Files", // someArr[0]
+  "The Dresden Files", // someArr[1]
+  "Rivers of London", // someArr[2]
+  "The Culture", // someArr[3]
+];
+
+let laundryFiles = [
+  {
+    title: "A Conventional Boy",
+    author: "Charles Stross",
+    pages: 211,
+    year_published: 2024,
+  },
+  {
+    title: "Dead Not Dreaming",
+    author: "Charles Stross",
+    pages: 211,
+    year_published: 2022,
+  },
+  {
+    title: "The Fuller Memorandum",
+    author: "Charles Stross",
+    pages: 211,
+    year_published: 2015,
+  },
+];
