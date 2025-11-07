42d54840052527c346913c624889bf495788f9d2
Shane B. <dotfortun@users.noreply.github.com>
2025-11-07 23:56:05 +0000

    Updated bootstrap, displayed random book on page.

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let laundryFiles = [
+let books = [
   {
     title: "A Conventional Boy",
     author: "Charles Stross",
@@ -22,6 +22,24 @@ let laundryFiles = [
     pages: 211,
     year_published: 2015,
   },
+  {
+    title: "Where's Waldo",
+    author: "Martin Hanford",
+    pages: 211,
+    year_published: 2015,
+  },
+  {
+    title: "The Visual Display of Quantitaive Information",
+    author: "Edward R. Tufte",
+    pages: 211,
+    year_published: 2015,
+  },
+  {
+    title: "Dune",
+    author: "Frank Herbert",
+    pages: 687,
+    year_published: 2015,
+  },
 ];
 
 /**
@@ -32,6 +50,63 @@ let laundryFiles = [
  * the HTML document (aka the Document Object Model.)
  */
 
+// Arrow/Anonymous function
+/**
+ * This function accepts a book object
+ * and return a string describing the book
+ * in this format:
+ * 
+ * {title} - {year_published}
+ * - By: {author}
+ * - {pages} pgs
+ */
+const describeBook = (book) => {
+  // Any code you write in the squiggly braces
+  // is part of the function.
+  const bookDescription = `${book.title} - ${book.year_published}
+- By: ${book.author}
+- ${book.pages} pgs`
+
+  return bookDescription;
+}
+
+function makeBookCard(book) {
+  const bookHTML = `
+<div class="card mx-auto mb-2" style="max-width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${book.title} - ${book.year_published}</h5>
+    <ul>
+      <li>
+        By: ${book.author}
+      </li>
+      <li>
+        ${book.pages} pgs
+      </li>
+    </ul>
+  </div>
+</div>
+`
+  return bookHTML;
+}
+
+/*
+ * These are really cool, and we'll probably talk about 
+ * them later on in the course (but only for a bit.)
+ */
+function* generatorFunc(books) {
+  // This is a special type of function
+  // You can iterate over these (but they are more work.)
+  for (const book of books) {
+    yield book
+  }
+}
+
 window.onload = function() {
-  document.body.innerHTML = "<h1>Hello world (from JS).</h1>";
+  const booksDiv = document.querySelector("#books");
+  const randomBook = books[
+    Math.floor(Math.random() * books.length)
+  ];
+  booksDiv.innerHTML += makeBookCard(randomBook);
+  // This is logging the aria-description property.
+  console.log(booksDiv.ariaDescription);
 };
