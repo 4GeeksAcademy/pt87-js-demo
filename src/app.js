7e77f9736c9a82132de2578d7470914e58a75a11
Shane B. <dotfortun@users.noreply.github.com>
2025-11-17 23:48:10 +0000

    Updated component functions to be more like components.

--- src/app.js
+++ src/app.js
@@ -6,57 +6,104 @@ import "./assets/img/4geeks.ico";
 import json_data from "./data";
 
 window.onload = () => {
+  // Setting up pagination.
   let page_size = 3;
-  let page = 0;
+  let page_num = 0;
 
-  function makeBookCard(book) {
-    const bookHTML = `
-<div class="card mx-auto mb-2" style="max-width: 18rem;">
-  <img src="${
-    book.cover ? book.cover : "https://placehold.co/300x500/888/888/"
-  }" class="card-img" alt="...">
-  <div class="card-img-overlay text-bg-dork">
-    <h5 class="card-title">${book.title} - ${book.year_published}</h5>
-    <p>
-      By: ${book.author} <br />
-      ${book.num_pages} pgs
-    </p>
-  </div>
-</div>
-`;
-    return bookHTML;
+  const Button = (bg_color="primary", icon="fa-book-skull", isToggled=true) => {
+    return `
+    <button class="btn bg-${bg_color} bg-gradient text-dark">
+      ${
+        isToggled ? '<i class="fa-solid fa-thumbs-up"></i>'
+        : '<i class="fa-solid fa-thumbs-down"></i>'
+      }
+      <i class="fa-solid ${icon}"></i>
+    </button>`
   }
 
+  /**
+   * Generates HTML for a book card based on a book object.
+   *
+   * @param {*} book
+   * @returns str
+   */
+  const BookCard = (book) => {
+    return `
+      <div class="card mx-auto mb-2">
+        <img src="${
+          // Ternary operator!  AKA in-line if statement.
+          // question ? what to show if true : what to show if false.
+          book.cover ? book.cover : "https://placehold.co/300x500/888/888/"
+        }" class="card-img"  style="max-height: 75dvh" alt="...">
+      <div class="card-img-overlay text-bg-dork">
+        <div class="card-body">
+          <h5 class="card-title">${book.title} ${book.year_published ? "&mdash; " + book.year_published : ""}</h5>
+        </div>
+        <ul class="list-group my-2">
+          <li>By: ${book.author}</li>
+          ${book.num_pages ? `<li>${book.num_pages} pgs</li>` : ""}
+          ${book.year_published ? `<li>Published ${book.year_published}</li>` : ""}
+          ${book.isbn10 ? `<li>ISBN-10: ${book.isbn10}</li>` : ""}
+          ${book.isbn13 ? `<li>ISBN-13: ${book.isbn13}</li>` : ""}
+        </ul>
+        <div class="card-footer d-flex justify-content-between mt-auto mb-2">
+          ${Button("info", "fa-book-skull", book.have_read)}
+          ${Button("danger", "fa-fire-burner", book.is_awesome)}
+        </div>
+      </div>
+    </div>`;
+  };
+
+  const page_up_button = document.querySelector("#page_up");
+  const page_down_button = document.querySelector("#page_down");
+
+  /**
+   * Draws book cards on the page, based on pagination.
+   */
   const drawCards = () => {
     const booksDiv = document.querySelector("#books");
 
     booksDiv.innerHTML = "";
 
     const book_slice = json_data.books.slice(
-      page * page_size,
-      page * page_size + page_size
+      page_num * page_size, // Index of page start.
+      page_num * page_size + page_size // Index of page end.
     );
 
     for (const book of book_slice) {
-      booksDiv.innerHTML += makeBookCard(book);
+      booksDiv.innerHTML += BookCard(book);
     }
 
-    document.querySelector("#page-badge").innerHTML = page + 1;
+    document.querySelector("#page-num-badge").innerHTML = page_num + 1;
+
+    if (page_num <= 0) {
+      page_down_button.disabled = true;
+      page_down_button.classList.replace("btn-primary", "btn-secondary");
+    } else if (page_num < Math.floor(json_data.books.length / page_size)) {
+      page_down_button.disabled = false;
+      page_down_button.classList.replace("btn-secondary", "btn-primary");
+      page_up_button.disabled = false;
+      page_up_button.classList.replace("btn-secondary", "btn-primary");
+    } else {
+      page_up_button.disabled = true;
+      page_up_button.classList.replace("btn-primary", "btn-secondary");
+    }
   };
 
-  document.querySelector("#incr").onclick = () => {
-    page++;
-    if (page > Math.floor(json_data.books.length / page_size)) {
-      page = Math.floor(json_data.books.length / page_size);
+  // Adding interactivity to the pagination buttons.
+  page_up_button.addEventListener("click", () => {
+    page_num++;
+    if (page_num > Math.floor(json_data.books.length / page_size)) {
+      page_num = Math.floor(json_data.books.length / page_size);
     }
     drawCards();
-  };
+  });
 
-  // Event listeners just allow you more flexibility!
-  document.querySelector("#decr").addEventListener("click", () => {
-    page--;
-    if (page < 0) {
-      page = 0;
+  // Adding interactivity to the pagination buttons.
+  page_down_button.addEventListener("click", () => {
+    page_num--;
+    if (page_num < 0) {
+      page_num = 0;
     }
     drawCards();
   });
